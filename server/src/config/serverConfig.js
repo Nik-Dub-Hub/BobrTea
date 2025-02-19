ff1783b3b0eaf78b614c0a7563fe4ed60f411725
Nik-Dub-Hub <ioi741741@gmail.com>
2025-02-19 14:30:52 +0300

    start settings1.1

--- server/src/config/serverConfig.js
+++ server/src/config/serverConfig.js
@@ -2,16 +2,22 @@ const express = require("express");
 const removeHTTPHeader = require("../middleware/removeHeader");
 const morgan = require("morgan");
 const path = require("path");
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
-
+const corsOptions = {
+  origin: [process.env.CLIENT_URL],
+  credentials: true,
+};
 
 const serverConfig = (app) => {
   app.use("/static", express.static(path.resolve(__dirname, "..", "public")));
   app.use(express.urlencoded({ extended: true }));
-
+  app.use(cors(corsOptions));
   app.use(express.json());
   app.use(removeHTTPHeader); 
   app.use(morgan("dev"));
+  app.use(cookieParser());
 };
 
 module.exports = serverConfig;
