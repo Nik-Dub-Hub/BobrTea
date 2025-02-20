f7a04450c41afdff542b622afedfaf6f11245193
VovchikWes <mr.bz@list.ru>
2025-02-20 14:17:20 +0300

    customize display of tea by id

---
+++ client/src/entities/tea/TeaApi.js
@@ -0,0 +1,23 @@
+import { axiosInstance } from '../../shared/lib/axiosInstance';
+
+export class TeaApi {
+  static async getAll() {
+    const { data } = await axiosInstance.get('/tea');
+    return data;
+  }
+
+  static async getById(id) {
+    const { data } = await axiosInstance.get(`/tea/${id}`);
+    return data;
+  }
+
+  static async create(inputs) {
+    const { data } = await axiosInstance.post('/tea', inputs);
+    return data;
+  }
+
+  static async delete(id) {
+    const { data } = await axiosInstance.delete(`/tea/${id}`);
+    return data;
+  }
+}
