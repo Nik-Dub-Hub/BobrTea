9baf1e495f7c2144b8d22f7d3e8bfbfc1443d338
luckytoxic <mrbalumba27@gmail.com>
2025-02-21 15:33:46 +0300

    simple fix

--- client/src/widgets/CommentList/CommentList.jsx
+++ client/src/widgets/CommentList/CommentList.jsx
@@ -1,17 +1,20 @@
-import CommentCard from "../CommentCart/CommentCart";
+import CommentCard from "../CommentCard/CommentCard"
 import CommentForm from "../CommentForm/CommentForm";
 
-export default function CommentList({comments,id,user, setComments}){
-    return(
-                <>
-                <CommentForm id={id} setComments={setComments}/>
-                <div>
-                
-                {
-                    comments.map((comment) => (
-                        <CommentCard comment={comment} key={comment.id} user={user} setComments={setComments}/> )) 
-                }
-                </div>
-                </>
-    )
+export default function CommentList({ comments, id, user, setComments }) {
+  return (
+    <>
+      <CommentForm id={id} setComments={setComments} />
+      <div>
+        {comments.map((comment) => (
+          <CommentCard
+            comment={comment}
+            key={comment.id}
+            user={user}
+            setComments={setComments}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
